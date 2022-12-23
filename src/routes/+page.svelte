8b28acf3d498e9bbdfe2b34a3b7adccf112efd5f
Jeffrey Biles <bilesjeffrey@gmail.com>
2022-12-23 07:56:26 -0600

    Switch to gradients

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,41 @@
 <script>
   import AccomplishmentBanner from "./AccomplishmentBanner.svelte";
 
-  import meshGradientExample from "$lib/assets/mesh-gradient-example.png";
   import squirrellockHolmes from "$lib/assets/squirrellock-holmes.png";
+  import vueLogo from '$lib/assets/vue-logo.png';
+  import cartMan from '$lib/assets/cart-man.png';
 </script>
 
-<AccomplishmentBanner
-  title="SQuirrelLock Holmes"
-  description="Learn SQL with a fun game"
-  backgroundImage={meshGradientExample}
-  foregroundImage={squirrellockHolmes}
-/>
+<div class="banner-list">
+  <AccomplishmentBanner
+    title="SQuirrelLock Holmes"
+    description="Learn SQL with a fun game"
+    foregroundImage={squirrellockHolmes}
+    gradient="linear-gradient(45deg, rgba(0,0,0,1) 14%, rgba(212,100, 0,1) 67%)"
+    
+  />
+  <AccomplishmentBanner
+    title="VueScreencasts"
+    description="In-depth tutorial screencasts about VueJS and Nuxt"
+    gradient="linear-gradient(45deg, rgba(0,0,0,1) 14%, rgba(9,185,132,1) 67%)"
+    foregroundImage={vueLogo}
+  />
+  <AccomplishmentBanner
+    title="E(vil)-Commerce"
+    description="Introductory screencasts for Next 13, Nuxt 3, and SvelteKit 1.0.  We build an e-commerce app with a twist!"
+    wideDescription={true}
+    gradient="linear-gradient(45deg, rgba(0,0,0,1) 14%, rgba(230,230,3,1) 67%)"
+    foregroundImage={cartMan}
+  />
+</div>
+
+<style>
+  .banner-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .banner-list > :global(*) {
+    margin: 8px;
+  }
+</style>
--- src/routes/AccomplishmentBanner.svelte
+++ src/routes/AccomplishmentBanner.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   export let title: string = '';
   export let description: string;
-  export let backgroundImage: any;
   export let foregroundImage: any;
+  export let wideDescription: boolean = false;
+  export let gradient: string;
 </script>
 
-<div class="accomplishment-banner">
-  <img class="background" src={backgroundImage} />
+<div class="accomplishment-banner" style="background: {gradient};">
   <img class="foreground" src={foregroundImage} />
   <div class="content">
     <div class="title"><div>{title}</div></div>
-    <div class="description"><div>{description}</div></div>
+    <div class="description {wideDescription ? 'wide-description' : ''}"><div>{description}</div></div>
   </div>
 </div>
 
@@ -19,11 +19,12 @@
   .accomplishment-banner {
     height: 130px;
     width: 400px;
-    border: 1px solid black;
-    position: absolute;
+    border: 3px solid black;
     transform: skewX(-20deg);
     overflow: hidden;
     cursor: pointer;
+    /* box-shadow: 0 0 30px 10px rgba(0, 0, 0, 0.5); */
+    
   }
   .content {
     width: 100%;
@@ -48,18 +49,15 @@
   .description {
     padding: 4px 8px;
     background-color: #0F1111;
+    max-width: 200px;
+    text-align: center;
   }
-  img.background {
-    width: 100%;
-    height: 100%;
-    object-position: center; /* Center the image within the element */
-    object-fit: cover;
-    background-color: #0F1111;
-    position: absolute;
-    z-index: -1;
+  .wide-description {
+    max-width: 300px;
   }
   img.foreground {
     top: 70px;
+    left: -30px;
     width: 150px;
     position: absolute;
     z-index: 1;
@@ -70,6 +68,6 @@
     top: 40px;
   }
   .accomplishment-banner:hover .content {
-    margin-left: 40px;
+    margin-left: 20px;
   }
 </style>
